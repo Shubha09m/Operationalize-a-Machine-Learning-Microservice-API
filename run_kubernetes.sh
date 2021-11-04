80edc88b4bcbd9df56cb4c47c93a30098de0d26c
shubha09m <shubham.bit241@gmail.com>
2021-11-04 23:48:41 +0530

    K8s output file added

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,10 +9,7 @@ dockerpath="shubham241/mlapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run mlmicroserviceapi\
-    --generator=run-pod/v1\
-    --image=$dockerpath\
-    --port=80 --labels app=mlmicroserviceapi
+kubectl run ml-microservice --image=$dockerpath --port=80 --labels="app=ml-microservice"
 
 # Step 3:
 # List kubernetes pods
@@ -20,4 +17,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward mlmicroserviceapi 8000:80
+export POD_NAME=$(kubectl get pod -l app=ml-microservice -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8000:80
